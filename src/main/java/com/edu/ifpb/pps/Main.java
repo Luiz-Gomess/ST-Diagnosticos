036bd81b9bccda987ad830455a005f949aa96390
Luiz-Gomess <Lf17353@gmail.com>
2025-07-04 11:12:56 -0300

    implementando exportacao por html

--- src/main/java/com/edu/ifpb/pps/Main.java
+++ src/main/java/com/edu/ifpb/pps/Main.java
@@ -1,9 +1,20 @@
 package com.edu.ifpb.pps;
 
+import com.edu.ifpb.pps.exames.Exame;
+import com.edu.ifpb.pps.exames.impl.Hemograma;
+import com.edu.ifpb.pps.exames.impl.Ressonancia;
+import com.edu.ifpb.pps.laudos.impl.GeradorHTML;
+import com.edu.ifpb.pps.validacoes.Validador;
+
 public class Main {
     public static void main(String[] args) {
 
-        // Exame exame = new Hemograma( 10.3, 12.5, 40.0);
+        // Exame exame = new Hemograma(new GeradorHTML(), 10.3, 12.5, 40.0);
+        Exame exame = new Hemograma(new GeradorHTML(), 10, 12, 40);
+        exame.gerarLaudo();
+        
+        exame = new Ressonancia(new GeradorHTML());
+        exame.gerarLaudo();
 
         // Validador validadorHemograma = new ExecessoValoresHemoglobinas();
 
--- src/main/java/com/edu/ifpb/pps/laudos/impl/GeradorHTML.java
+++ src/main/java/com/edu/ifpb/pps/laudos/impl/GeradorHTML.java
@@ -1,6 +1,12 @@
 package com.edu.ifpb.pps.laudos.impl;
 
-import java.util.Map;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 import com.edu.ifpb.pps.exames.Exame;
 import com.edu.ifpb.pps.laudos.GeradorDeLaudo;
@@ -10,31 +16,34 @@ public class GeradorHTML implements GeradorDeLaudo {
 
     @Override
     public String gerarLaudo(Exame exame) {
-
+        
         CorpoExame dados = exame.getDescricaoExame();
-        
-        StringBuilder sb = new StringBuilder();
-        sb.append("<html>\n");
-        sb.append("<head>\n");
-        sb.append("<title>Exame de " +dados.titulo + " </title>\n");
-        sb.append("</head>\n");
-        sb.append("<body>\n");
-        sb.append("<h1>" + dados.titulo + "</h1>\n");
-        sb.append("<p>" + dados.descricao + "</p>\n");
-        sb.append("<h2>Detalhes do Exame</h2>\n");
 
-        for (String linha : (Iterable<String>) dados.listaItens) {
-            sb.append("<p>" + linha + "</p>\n");
+        ClassLoaderTemplateResolver resolver = new ClassLoaderTemplateResolver();
+        resolver.setPrefix("templates/"); 
+        resolver.setSuffix(".html");       
+        resolver.setCharacterEncoding("UTF-8");
+        resolver.setTemplateMode("HTML"); 
+
+        TemplateEngine engine = new TemplateEngine();
+        engine.setTemplateResolver(resolver);
+
+        Context context = new Context();
+        context.setVariable("imagens", dados.imagens); 
+        context.setVariable("titulo", dados.titulo); 
+        context.setVariable("descricao", dados.descricao); 
+        context.setVariable("listaItems", dados.listaItens); 
+
+        String htmlGerado = engine.process("exame", context);
+
+        try (PrintWriter writer = new PrintWriter(new FileWriter("exame_preenchido.html"))) {
+            writer.println(htmlGerado);
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-        for (Map<String, String> imagem : dados.imagens) {
-            sb.append("<img src=\"" + imagem.get("src") + "\" alt=\"" + imagem.get("alt") + "\" />\n");
-        }
-        
-        sb.append("</body>\n");
-        sb.append("</html>\n");
-
-        return sb.toString();
+        return "ok";
     }
     
 }
